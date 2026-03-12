68830cf1f57f28505a2193150fc09fcc749555a6
MRTitus02 <titus.senthilkumaran@teceze.com>
2026-03-12 19:02:21 +0530

    fixed eq issue

--- src/repositories/items.repositories.ts
+++ src/repositories/items.repositories.ts
@@ -1,11 +1,20 @@
-import { db } from "../db/client.ts";
-import { items } from "../db/schema.ts";
+import { db } from "../db/client.js";
+import { items } from "../db/schema.js";
+import { eq } from "drizzle-orm";
 
 export const itemsRepository = {
   getAll: () => db.select().from(items),
+
   create: ({ title, description, userId }: any) =>
     db.insert(items).values({ title, description, userId }).returning(),
+
   update: (id: number, data: any) =>
-    db.update(items).set(data).where(items.id.eq(id)).returning(),
-  delete: (id: number) => db.delete(items).where(items.id.eq(id)),
+    db.update(items)
+      .set(data)
+      .where(eq(items.id, id))
+      .returning(),
+
+  delete: (id: number) =>
+    db.delete(items)
+      .where(eq(items.id, id))
 };
